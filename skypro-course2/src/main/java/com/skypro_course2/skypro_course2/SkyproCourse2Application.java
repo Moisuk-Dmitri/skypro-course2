00520fbcbf4803e6fd3376e1ea0830a7dc4ded95
Moisuk-Dmitri <moisuk.d@gmail.com>
2024-10-12 19:26:25 +0300

    Initial commit

---
+++ skypro-course2/src/main/java/com/skypro_course2/skypro_course2/SkyproCourse2Application.java
@@ -0,0 +1,13 @@
+package com.skypro_course2.skypro_course2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCourse2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCourse2Application.class, args);
+	}
+
+}
